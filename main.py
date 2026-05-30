768d9dfb94f4d6b98f602cf8041ab799ba1497a0
sandeepmohamed666 <sandeepmohamed666@gmail.com>
2026-05-30 13:34:12 +0530

    k

--- main.py
+++ main.py
@@ -6,4 +6,11 @@ async def root():
     return {"message": "Hello World"}
 
 # pip install "fastapi[standard]"
+
+
+# # Install requirements
+# python -m pip install --upgrade pip
+# pip install "fastapi[standard]"
+
+# # Run (Execute)
 # fastapi dev main.py
